e9a7bf9c1ac44694ec3e198ec477339493cb0539
Nancy Jo Chang <enciie@gmail.com>
2018-03-30 19:02:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function theBeatlesPlay(musicians, instruments){
+  var array= [];
+  for(var i = 0; i <musicians.length; i++){
+    array.push(musicians[i] + " plays " + instruments[i])
+  }
+  return array;
+}
+function johnLennonFacts(){
+  var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+  var i = 0;
+  while(i<facts.length){
+    facts.push(facts[i]+"!!!");
+    i++;
+  }
+  return facts;
+}
